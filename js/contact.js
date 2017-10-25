1f8800097844915b7ba55ac742838ac4e377fbc5
benreckas <benreckas@gmail.com>
2017-10-25 03:51:19 +0000

    Fixed JavaScript bug in Contact Form.

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,11 @@
 // Form Submit Confirmation
 
-(function() {
-    function clicked (event) {
-        event.preventDefault();
-        console.log('clicked');
-        const formConfirm = document.getElementById("form-confirmation");
-        const formSubmit = document.getElementById("btn");
-        const userName = document.getElementById("first-name").value;
-        formConfirm.innerHTML = "Thanks for your feedback " + userName + "!";
-        console.log(userName);
-    };
-}());
+function clicked (event) {
+    event.preventDefault();
+    console.log('clicked');
+    const formConfirm = document.getElementById("form-confirmation");
+    const formSubmit = document.getElementById("btn");
+    const userName = document.getElementById("first-name").value;
+    formConfirm.innerHTML = "Thanks for your feedback " + userName + "!";
+    console.log(userName);
+};
